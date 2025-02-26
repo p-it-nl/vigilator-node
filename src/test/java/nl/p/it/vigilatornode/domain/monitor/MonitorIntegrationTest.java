5c7ce2cd8659957ea6912ac3f13c6bc4d728ed36
Patrick Aleman - Maan <patrick.aleman@protonmail.com>
2025-02-26 17:22:51 +0100

    upgrade tests to properly handle invalid URLs and other

--- src/test/java/nl/p/it/vigilatornode/domain/monitor/MonitorIntegrationTest.java
+++ src/test/java/nl/p/it/vigilatornode/domain/monitor/MonitorIntegrationTest.java
@@ -43,6 +43,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Tests for monitor
